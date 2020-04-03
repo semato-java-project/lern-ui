7a7facf6cedf156da195dfb0733327211e3fadbb
dereypl <mpasich.pk@gmail.com>
2020-04-03 11:07:22 +0200

    - Add publication section code refactor.

---
+++ src/components/organisms/Course/AddPublicationContainer.js
@@ -0,0 +1,74 @@
+import React from "react";
+import styled from "styled-components";
+import Heading from "../../atoms/Headings/Heading";
+import {AddPublicationImage} from "../../molecules/Images/AddPublicationImage";
+import Button from "../../atoms/Button/Button";
+
+const StyledButton = styled(Button)`
+    margin: 3rem;
+    width: 18rem;
+    min-height: 1rem;
+    height: 4rem;
+    font-weight: ${({theme}) => theme.fontWeight.regular};
+    background-color: ${({theme}) => theme.app_yellow};
+    color: ${({theme}) => theme.app_blue_dark};
+    
+    &:hover{
+      background-color: ${({theme}) => theme.app_blue_dark};
+      }
+`;
+
+const AddPublicationWrapper = styled.div`
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      height: 30%;
+      position:relative;
+      align-items: center;
+`;
+
+const AddPublicationImageWrapper = styled.div`
+      display: flex;
+      width: 60%;
+      margin-left: -12rem;
+`;
+
+const AddPublicationTextWrapper = styled.div`
+      display: flex;
+      width: 70%;
+      align-content: center;
+      text-align: center;
+`;
+const AddPublicationTitle = styled(Heading)`
+      display:flex;
+      flex-direction: column;
+      position:absolute;
+      top: 7.5rem;
+      left: 40%;
+      font-size: 2.2rem;
+      color: ${({theme}) => theme.app_blue_light};
+      span{
+        font-weight: ${({theme}) => theme.fontWeight.bold};
+      }
+`;
+
+
+const AddPublicationContainer = () => {
+
+    return (
+        <AddPublicationWrapper>
+            <AddPublicationImageWrapper>
+                <AddPublicationImage/>
+            </AddPublicationImageWrapper>
+            <AddPublicationTitle>
+                Nie zapomnij<span>dodać publikacji!</span>
+            </AddPublicationTitle>
+            <AddPublicationTextWrapper>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </AddPublicationTextWrapper>
+            <StyledButton>Dodaj publikację{'>'}</StyledButton>
+        </AddPublicationWrapper>
+    )
+};
+
+export default AddPublicationContainer;
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -5,9 +5,7 @@ import {HorizontalSeparator} from "../components/atoms/Shapes/HorizontalSeparato
 import LastCourseContainer, {LastCourseWrapper} from "../components/organisms/Course/LastCourseContainer";
 import AddCourseContainer from "../components/organisms/Course/AddCourseContainer";
 import CourseStats, {StatsWrapper} from "../components/organisms/Stats/CourseStats";
-import {AddPublicationImage} from "../components/molecules/Images/AddPublicationImage";
-import Heading from "../components/atoms/Headings/Heading";
-import Button from "../components/atoms/Button/Button";
+import AddPublicationContainer from "../components/organisms/Course/AddPublicationContainer";
 
 const HeaderPathInfoContainer = styled.div`
       display: flex;
@@ -50,39 +48,6 @@ const SideContentSection = styled.div`
       padding: 2rem 3rem 0;
 `;
 
-const AddPublicationContainer = styled.div`
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      height: 30%;
-      position:relative;
-      align-items: center;
-`;
-
-const AddPublicationImageWrapper = styled.div`
-      display: flex;
-      width: 60%;
-      margin-left: -12rem;
-`;
-
-const AddPublicationTextWrapper = styled.div`
-      display: flex;
-      width: 70%;
-      align-content: center;
-      text-align: center;
-`;
-const AddPublicationTitle = styled(Heading)`
-      display:flex;
-      flex-direction: column;
-      position:absolute;
-      top: 7.5rem;
-      left: 40%;
-      font-size: 2.2rem;
-      color: ${({theme}) => theme.app_blue_light};
-      span{
-        font-weight: ${({theme}) => theme.fontWeight.bold};
-      }
-`;
 
 const LastCoursesSection = styled.section`
       display: flex;
@@ -110,20 +75,6 @@ const HorizontalTitle = styled.div`
       margin-bottom: 2vh;
 `;
 
-const StyledButton = styled(Button)`
-    margin: 3rem;
-    width: 18rem;
-    min-height: 1rem;
-    height: 4rem;
-    font-weight: ${({theme}) => theme.fontWeight.regular};
-    background-color: ${({theme}) => theme.app_yellow};
-    color: ${({theme}) => theme.app_blue_dark};
-    
-    &:hover{
-      background-color: ${({theme}) => theme.app_blue_dark};
-      }
-`;
-
 
 const Dashboard = () => {
 
@@ -148,18 +99,7 @@ const Dashboard = () => {
                     </StatsSection>
                 </MainContentSection>
                 <SideContentSection>
-                    <AddPublicationContainer>
-                        <AddPublicationImageWrapper>
-                            <AddPublicationImage/>
-                        </AddPublicationImageWrapper>
-                        <AddPublicationTitle>
-                            Nie zapomnij<span>dodać publikacji!</span>
-                        </AddPublicationTitle>
-                        <AddPublicationTextWrapper>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        </AddPublicationTextWrapper>
-                        <StyledButton>Dodaj publikację{'>'}</StyledButton>
-                    </AddPublicationContainer>
+                    <AddPublicationContainer/>
                 </SideContentSection>
             </ContentWrapper>
         </SidebarTemplate>
