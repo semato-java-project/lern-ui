031f922d3285167a2f9e22de9bbfff89283844e5
dereypl <mpasich.pk@gmail.com>
2020-05-31 14:42:26 +0200

    Token validation improvements.

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,7 +6,7 @@ import {USER_ROLES} from "../utils/types";
 // --- TOKEN ---
 const AUTH_TOKEN = 'AUTH_TOKEN';
 export const getToken = () => localStorage.getItem(AUTH_TOKEN);
-export const isTokenValid = () => getToken() && jwtDecode(getToken()).sub === 'semato-learn-project';
+export const isTokenValid = token => token && jwtDecode(token).sub === 'semato-learn-project';
 const getJWTUser = token => jwtDecode(token).userJwtInfo;
 
 // --- LOGIN / LOGOUT ---
@@ -22,15 +22,24 @@ export const authenticate = (email, password) => async dispatch => {
     dispatch({type: ACTION_TYPES.AUTHENTICATION_REQUEST});
 
     try {
+
+        // --- API REQUEST ---
         let {data: token} = await authRequest(email, password);
+
+        // --- BASIC TOKEN VALIDATION ---
+        if (!isTokenValid(token)) throw new Error("Token validation error");
+
+        // --- HANDLE AUTH SUCCESS ---
         dispatch({type: ACTION_TYPES.AUTHENTICATION_SUCCESS, payload: getJWTUser(token)});
         logInUser(token);
+
+        // --- RETURN USER ROLE & FETCH DATA IF NEEDED ---
         let userRole = getJWTUser(token).role;
         if (userRole === USER_ROLES.ROLE_LECTURER.API_NAME) dispatch(getList(requestTypes.GET_COURSES));
         dispatch(getList(requestTypes.GET_NEWS));
         return userRole;
 
-    } catch (ex) {
+    } catch (err) {
         dispatch({type: ACTION_TYPES.AUTHENTICATION_FAILURE});
         return null;
     }
--- src/utils/routes/PrivateRoute.js
+++ src/utils/routes/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 import {routes} from './index';
 import {useSelector} from "react-redux";
-import {isTokenValid} from "../../services/userService";
+import {getToken, isTokenValid} from "../../services/userService";
 
 const PrivateRoute = ({component: Component, acceptedRoles, ...restProps}) => {
 
@@ -13,7 +13,7 @@ const PrivateRoute = ({component: Component, acceptedRoles, ...restProps}) => {
         <Route
             {...restProps}
             render={props => {
-                return isUserLogged && isTokenValid() && acceptedRoles.includes(role) ? (
+                return isUserLogged && isTokenValid(getToken()) && acceptedRoles.includes(role) ? (
                     <Component {...props} />
                 ) : (
                     <Redirect to={{pathname: routes.HOME, state: {from: props.location}}}/>
